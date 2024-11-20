ee67295b2824d05514b6a86940005c8d52e994fc
Graceli95 <graceli9095@gmail.com>
2024-11-20 16:48:41 -0700

    add files

--- src/Example2.java
+++ src/Example2.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Example2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        String s = input.nextLine();
+        char ch = s.charAt(1);
+        System.out.println(ch);
+
+
+
+    }
 }
